32a6a96d33686ecdaf69d7081bced4884232b40d
NDuong <139540304+nduong18@users.noreply.github.com>
2025-06-10 17:57:57 +0700

    Create CB04 - Tính tổng, hiệu, tích, thương của 2 số nguyên.py

---
+++ CB04 - Tính tổng, hiệu, tích, thương của 2 số nguyên.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+
+if b != 0:
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print('%.2f' % float(a/b))
+else:
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print('ERROR')
